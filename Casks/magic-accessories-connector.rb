ba0c75365661667a61e7d4fa0b6b2d6a7c873721
Dborasik <dborasik@users.noreply.github.com>
2026-06-03 17:35:57 -0400

    magic-accessories-connector 1.2.1

--- Casks/magic-accessories-connector.rb
+++ Casks/magic-accessories-connector.rb
@@ -2,8 +2,8 @@
 # frozen_string_literal: true
 
 cask "magic-accessories-connector" do
-  version "1.2.0"
-  sha256 "5ab36f7cd5fc3c2fcc29d62e3360962fa6ff6bed655512c242c2f0294f49003c"
+  version "1.2.1"
+  sha256 "1a09fd23d4119e64c65dc8f06bb0698393296475d28d2fb11790ab45c0678d7f"
 
   url "https://github.com/Radixen-Dev/MagicAccessoriesConnector/releases/download/v#{version}/MagicAccessoriesConnector-#{version}.zip"
   name "Magic Accessories Connector"
